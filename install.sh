6631933311bbbec44f6b201b5bc285ffbbecb74a
hirsch-b <hirschfi.b@gmail.com>
2021-03-24 15:55:18 +0100

    Improved the installation and add VSCode settings

--- install.sh
+++ install.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+CONFIG_VSCODE="~/.config/Code/User/"
+
 sudo apt-get install -y \
     wget curl \
     git vim
 
+# Home setup
+pushd "$(dirname "${BASH_SOURCE[0]}")"
 cp ./bash_aliases ~/.bash_aliases
 cp ./vimrc ~/.vimrc
 cp ./vim ~/.vim -rv
 cp ./gitconfig ~/.gitconfig
 
+# VS Code setup
+mkdir -p "${CONFIG_VSCODE}"
+cp "./vscode/settings.json" "${CONFIG_VSCODE}/settings.json"
+cp "./vscode/keybindings.json" "${CONFIG_VSCODE}/keybindings.json"
+
+# End
+echo "You are ready"
+popd
